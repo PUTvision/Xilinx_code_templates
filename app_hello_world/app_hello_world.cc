5deed8490000c049af8e288c98b1ad98ccc99e36
Michał Fularz <michal.fularz@gmail.com>
2015-03-25 13:27:44 +0100

    Code for simple app in C++ for Zynq.

---
+++ app_hello_world/app_hello_world.cc
@@ -0,0 +1,23 @@
+/*
+ * Author: Michal Fularz
+ */
+
+#include <stdio.h>
+#include <xil_printf.h>
+#include "platform/platform.h"
+
+int main()
+{
+		init_platform();
+		
+		xil_printf("Hello World Zynq C++ bare metal application!\r\n");
+	
+		while(1)
+	{
+		asm("nop");
+	}
+	
+	cleanup_platform();
+	
+	return 0;
+}
