6a167c9ddb25164c5466f06ee6041ed48f4953a7
Michał Fularz <michal.fularz@gmail.com>
2015-04-08 13:28:33 +0200

    Minor formatting changes.

--- app_hello_world/app_hello_world.cc
+++ app_hello_world/app_hello_world.cc
@@ -8,11 +8,11 @@
 
 int main()
 {
-		init_platform();
+	init_platform();
 		
-		xil_printf("Hello World Zynq C++ bare metal application!\r\n");
+	xil_printf("Hello World Zynq C++ bare metal application!\r\n");
 	
-		while(1)
+	while(1)
 	{
 		asm("nop");
 	}
