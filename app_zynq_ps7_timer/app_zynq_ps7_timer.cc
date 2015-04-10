346c858cff11c5b225035f33de2302234aa681f4
Michał Fularz <michal.fularz@gmail.com>
2015-04-10 10:45:14 +0200

    Added some comments that are present in the blog post about the code.

--- app_zynq_ps7_timer/app_zynq_ps7_timer.cc
+++ app_zynq_ps7_timer/app_zynq_ps7_timer.cc
@@ -11,28 +11,36 @@
 
 static void TEST_zynq_ps7_timer_c_style(void)
 {
+	// init the timer by calling the function and passing the timer ID
 	TIMER_init(XPAR_PS7_SCUTIMER_0_DEVICE_ID);
 
 	TIMER_start();
 
+	// put code to measure its execution here
 	xil_printf("Using C   style!\r\n");
 
 	TIMER_stop();
 
+	// get elapsed time (from start to stop) in microseconds
 	float elapsedTime = TIMER_getTimeInUs();
 	printf("xil_printf took: %f us to execute\r\n", elapsedTime);
 }
 
 static void TEST_zynq_ps7_timer_cpp_style(void)
 {
+	// init the timer by creating class object and passing timer ID to the constructor
 	StopWatch sw(XPAR_PS7_SCUTIMER_0_DEVICE_ID);
 
+
+	// this time starting is done by using method of the class
 	sw.Start();
 
+	// put code to measure its execution here
 	xil_printf("Using C++ style!\r\n");
 
 	sw.Stop();
 
+	// get elapsed time (from start to stop) in microseconds
 	float elapsedTime = sw.GetTimeInUs();
 	printf("xil_printf took: %f us to execute\r\n", elapsedTime);
 }
